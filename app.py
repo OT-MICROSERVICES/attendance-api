e347903dbadf22ea6b4526eee2381a634bf72d05
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2023-07-03 22:26:14 +0530

    Added code for Prometheus Integration
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ Module for calling the main flask application.
 The application will be only supported with Flask and Gunicorn.
 """
 from flask import Flask, json
+from prometheus_flask_exporter import PrometheusMetrics
 from router.attendance import route as create_record
 from router.cache import cache
 from utils.json_encoder import DataclassJSONEncoder
 from client.redis.redis_conn import get_caching_data
 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info("attendance_api", "Attendance API opentelemetry metrics", version="0.1.0")
 
 cache.init_app(app, get_caching_data())
 
