06e7a534f80d7079ce188a97260cccf3f7eacd74
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2023-07-03 13:38:23 +0530

    Added code for json logging and healthcheck
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

---
+++ gunicorn_conf.py
@@ -0,0 +1,21 @@
+import json
+
+bind = '0.0.0.0:8080'
+workers = 3
+accesslog = '-'  # Send access logs to stdout
+
+access_log_format = json.dumps({
+    'remote_address': r'%(h)s',
+    'user_name': r'%(u)s',
+    'date': r'%(t)s',
+    'status': r'%(s)s',
+    'method': r'%(m)s',
+    'url_path': r'%(U)s',
+    'query_string': r'%(q)s',
+    'protocol': r'%(H)s',
+    'response_length': r'%(B)s',
+    'referer': r'%(f)s',
+    'user_agent': r'%(a)s',
+    'request_time_seconds': r'%(L)s',
+})
+
