66158167edcf2c701d3f5dc9b856f5326348898b
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2023-07-03 23:03:03 +0530

    Added swagger integration for API documentation
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ Module for calling the main flask application.
 The application will be only supported with Flask and Gunicorn.
 """
 from flask import Flask, json
+from flasgger import Swagger
 from prometheus_flask_exporter import PrometheusMetrics
 from router.attendance import route as create_record
 from router.cache import cache
@@ -10,6 +11,8 @@ from utils.json_encoder import DataclassJSONEncoder
 from client.redis.redis_conn import get_caching_data
 
 app = Flask(__name__)
+
+swagger = Swagger(app)
 
 metrics = PrometheusMetrics(app)
 metrics.info("attendance_api", "Attendance API opentelemetry metrics", version="0.1.0")
